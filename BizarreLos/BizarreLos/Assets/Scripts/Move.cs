f02d8f2a79b83c00210f3aa141bb168cbe8fca54
Daniel <kirovxq_@hotmail.com>
2016-06-24 02:04:36 -0500

    ojala funcuone

---
+++ BizarreLos/BizarreLos/Assets/Scripts/Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+
+	public float movespeed = 0.1f;
+
+	void Update ()
+	{
+		
+		if (Input.GetKey (KeyCode.RightArrow)) 
+			transform.position += new Vector3 (movespeed*Time.deltaTime, 0.0f, 0.0f);
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+			transform.position -= new Vector3 (movespeed*Time.deltaTime, 0.0f, 0.0f);
+	
+	}
+}
